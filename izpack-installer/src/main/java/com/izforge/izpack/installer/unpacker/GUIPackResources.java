ea9ecb77751c2a6a0402c36d3ca7641243bf2235
Fanzil Fattakhov <fattakhov@cg.ru>
2020-03-11 17:03:04 +0300

    WEBBPMNEXT-6769: fixed the creation of the path to the files for download

--- izpack-installer/src/main/java/com/izforge/izpack/installer/unpacker/GUIPackResources.java
+++ izpack-installer/src/main/java/com/izforge/izpack/installer/unpacker/GUIPackResources.java
@@ -47,8 +47,8 @@ public class GUIPackResources extends AbstractPackResources
         String baseName = installData.getInfo().getInstallerBase();
         File installerDir = new File(baseName).getParentFile();
 
-        if (baseName.contains("/"))
-            baseName = baseName.substring(baseName.lastIndexOf('/'));
+        if (baseName.contains(File.separator))
+            baseName = baseName.substring(baseName.lastIndexOf(File.separator) + 1);
 
         String packFileName = baseName + ".pack-" + name + ".jar";
         String path = null;
