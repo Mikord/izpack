d31308c15a4d196c2c622a23583a56940d8f5c96
Fanzil <fattakhov@cg.ru>
2020-06-01 11:36:51 +0300

    WEBBPMNEXT-7180: moved getWebPackStream method in abstract class

--- izpack-installer/src/main/java/com/izforge/izpack/installer/unpacker/AbstractPackResources.java
+++ izpack-installer/src/main/java/com/izforge/izpack/installer/unpacker/AbstractPackResources.java
@@ -26,8 +26,20 @@ import com.izforge.izpack.api.exception.ResourceException;
 import com.izforge.izpack.api.exception.ResourceInterruptedException;
 import com.izforge.izpack.api.exception.ResourceNotFoundException;
 import com.izforge.izpack.api.resource.Resources;
+import com.izforge.izpack.util.IoHelper;
+import com.izforge.izpack.util.OsVersion;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Logger;
 
 
 /**
@@ -37,6 +49,11 @@ import java.io.InputStream;
  */
 public abstract class AbstractPackResources implements PackResources
 {
+    /**
+     * The logger.
+     */
+    private static final Logger logger = Logger.getLogger(AbstractPackResources.class.getName());
+
     /**
      * Temporary directory for web installers.
      */
@@ -126,7 +143,82 @@ public abstract class AbstractPackResources implements PackResources
      * @throws ResourceNotFoundException    if the resource cannot be found
      * @throws ResourceInterruptedException if resource retrieval is interrupted
      */
-    protected abstract InputStream getWebPackStream(String name, String webDirURL);
+    protected InputStream getWebPackStream(String name, String webDirURL)
+    {
+        InputStream result;
+
+        InstallData installData = getInstallData();
+
+        String packFileName = name + ".jar";
+        String path = null;
+
+        // Look first in same directory as primary jar, then download it if not found
+        File packLocalFile = new File(installData.getVariable(InstallData.INSTALL_PATH), packFileName);
+        if (packLocalFile.exists() && packLocalFile.canRead())
+        {
+            logger.info("Found local pack " + packLocalFile.getAbsolutePath());
+            try {
+                path = "jar:" + packLocalFile.toURI().toURL() + "!/packs/pack-" + name;
+            } catch(MalformedURLException exception) {
+                throw new ResourceException("Malformed URL", exception);
+            }
+        }
+        else
+        {
+            String packURL = webDirURL + "/" + name.replace(" ", "%20");
+            logger.info("Downloading remote pack " + packURL);
+            String tempFolder = IoHelper.translatePath(installData.getInfo().getUninstallerPath()
+                                                       + WEB_TEMP_SUB_PATH, installData.getVariables());
+            File tempFile;
+            try
+            {
+                tempFile = new File(downloadPack(packURL, tempFolder, packFileName));
+
+                packLocalFile = Files.move(
+                    tempFile.toPath(),
+                    Paths.get(installData.getVariable(InstallData.INSTALL_PATH), tempFile.getName()),
+                    StandardCopyOption.REPLACE_EXISTING
+                ).toFile();
+            }
+            catch (InterruptedIOException exception)
+            {
+                throw new ResourceInterruptedException("Retrieval of " + webDirURL + " interrupted", exception);
+            }
+            catch (IOException exception)
+            {
+                throw new ResourceException("Failed to read " + webDirURL, exception);
+            }
+            catch (NoSuchAlgorithmException exception)
+            {
+                throw new ResourceException("Failed to get the checksum for " + webDirURL, exception);
+            }
+
+            String separator = "";
+            if (OsVersion.IS_WINDOWS) {
+                separator = "/";
+            }
+            path = "jar:file:" + separator + packLocalFile.getPath() + "!/packs/pack-" + name;
+        }
+
+        try
+        {
+            URL url = new URL(path);
+            result = url.openStream();
+        }
+        catch (IOException exception)
+        {
+            throw new ResourceException("Failed to read pack", exception);
+        }
+        return result;
+    }
+
+    /**
+     * downloads the pack and returns its location
+     *
+     * @return location of the downloaded pack
+     */
+    protected abstract String downloadPack(String url, String tempFolder, String packFileName)
+        throws IOException, NoSuchAlgorithmException;
 
     /**
      * Returns the installation data.
--- izpack-installer/src/main/java/com/izforge/izpack/installer/unpacker/ConsolePackResources.java
+++ izpack-installer/src/main/java/com/izforge/izpack/installer/unpacker/ConsolePackResources.java
@@ -22,19 +22,10 @@
 package com.izforge.izpack.installer.unpacker;
 
 import com.izforge.izpack.api.data.InstallData;
-import com.izforge.izpack.api.exception.ResourceException;
-import com.izforge.izpack.api.exception.ResourceInterruptedException;
 import com.izforge.izpack.api.resource.Resources;
 import com.izforge.izpack.installer.web.WebRepositoryAccessor;
-import com.izforge.izpack.util.IoHelper;
-import com.izforge.izpack.util.OsVersion;
 
 import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Logger;
 
@@ -61,76 +52,8 @@ public class ConsolePackResources extends AbstractPackResources
     }
 
     @Override
-    protected InputStream getWebPackStream(String name, String webDirURL)
-    {
-        InputStream result;
-
-        InstallData installData = getInstallData();
-
-        String packFileName = name + ".jar";
-        String path = null;
-
-        // Look first in same directory as primary jar, then download it if not found
-        File packLocalFile = new File(installData.getVariable(InstallData.INSTALL_PATH), packFileName);
-        if (packLocalFile.exists() && packLocalFile.canRead())
-        {
-            logger.info("Found local pack " + packLocalFile.getAbsolutePath());
-            try {
-                path = "jar:" + packLocalFile.toURI().toURL() + "!/packs/pack-" + name;
-            } catch(MalformedURLException exception) {
-                throw new ResourceException("Malformed URL", exception);
-            }
-        }
-        else
-        {
-            String packURL = webDirURL + "/" + name.replace(" ", "%20");
-            logger.info("Downloading remote pack " + packURL);
-            String tempFolder = IoHelper.translatePath(installData.getInfo().getUninstallerPath()
-                    + WEB_TEMP_SUB_PATH, installData.getVariables());
-            File tempFile;
-            try
-            {
-                tempFile = new File(
-                    WebRepositoryAccessor.getCachedUrl(packURL, tempFolder, packFileName, false)
-                );
-
-                packLocalFile = Files.move(
-                    tempFile.toPath(),
-                    Paths.get(installData.getVariable(InstallData.INSTALL_PATH), tempFile.getName()),
-                    StandardCopyOption.REPLACE_EXISTING
-                ).toFile();
-            }
-            catch (InterruptedIOException exception)
-            {
-                throw new ResourceInterruptedException("Retrieval of " + webDirURL + " interrupted", exception);
-            }
-            catch (IOException exception)
-            {
-                throw new ResourceException("Failed to read " + webDirURL, exception);
-            }
-            catch (NoSuchAlgorithmException exception)
-            {
-                throw new ResourceException("Failed to get the checksum for " + webDirURL, exception);
-            }
-            String startPath;
-            if (OsVersion.IS_WINDOWS) {
-                startPath = "jar:file:/";
-            }
-            else {
-                startPath = "jar:file:";
-            }
-            path = startPath + packLocalFile.getPath() + "!/packs/pack-" + name;
-        }
-
-        try
-        {
-            URL url = new URL(path);
-            result = url.openStream();
-        }
-        catch (IOException exception)
-        {
-            throw new ResourceException("Failed to read pack", exception);
-        }
-        return result;
+    protected String downloadPack(String packURL, String tempFolder, String packFileName)
+        throws IOException, NoSuchAlgorithmException {
+        return WebRepositoryAccessor.getCachedUrl(packURL, tempFolder, packFileName, false);
     }
 }
